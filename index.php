adad2c950356174703c95cee587435ad95154ba5
Adriano Germinal <adrianogerminal@gmail.com>
2026-03-21 18:17:59 +0100

    inizio esercizi php

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$Integer = 42;  
+$Float = 3.14;
+$String = "Ciao mi chiamo Adriano!";
+$Boolean = true;
+
+var_dump($Integer);
+var_dump($Float);   
+var_dump($String);
+var_dump($Boolean);
