714c4847c043bfa3499412a57dc7b7f06adbde7f
Adriano Germinal <adrianogerminal@gmail.com>
2026-03-31 16:16:31 +0200

    esercizio n 5

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 
 <?php
 
-$users = [
-  ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'NB'],
-  ['name' => 'Mario', 'surname' => 'Rossi', 'gender' => 'M'],
-  ['name' => 'Maria', 'surname' => 'Bianchi', 'gender' => 'F'],
-  ['name' => 'Adriano', 'surname' => 'Placido', 'gender' => 'NB']
-];
+$numeri=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,32, 45, 67, 89, 90];
 
-foreach ($users as $user) {
-  if ($user['gender'] == 'M') {
-    echo "Buongiorno Sig. " . $user['name'] . " " . $user['surname'] . "\n";
-  } elseif ($user['gender'] == 'F') {
-    echo "Buongiorno Sig.ra " . $user['name'] . " " . $user['surname'] . "\n";
-  } else {
-    echo "Buongiorno " . $user['name'] . " " . $user['surname'] . "\n";
-  }   ;
+//ho bisogno delle variabili che contengano la somma e il totale dei numeri pari 
+$somma=0;
+$totaleNumeriPari= 0;
+
+foreach($numeri as $numero){
+  if ($numero % 2 == 0){
+    $somma += $numero;
+    $totaleNumeriPari++;};
 };
+
+$media= $somma/$totaleNumeriPari; 
+
+echo "La media dei numeri pari è {$media}";
