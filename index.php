9c41eb0fd8e90a490e9c9e50bdb8d8e3a3e31eb1
Adriano Germinal <adrianogerminal@gmail.com>
2026-03-31 16:03:24 +0200

    esercizio 4

--- index.php
+++ index.php
@@ -1,46 +1,19 @@
+
 <?php
 
-// Dati i seguenti array:
-$words1 = [
-  'vostro',
-  67,
-  'essere',
-  'colle',
-  'mi',
-  'sempre',
-[
-  'oscura',
-  'era',
-  89,
-  [
-  'mezzo',
-   'E'
-  ],
-'ritrovai',
-'per'
-],
-'diritta'
+$users = [
+  ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'NB'],
+  ['name' => 'Mario', 'surname' => 'Rossi', 'gender' => 'M'],
+  ['name' => 'Maria', 'surname' => 'Bianchi', 'gender' => 'F'],
+  ['name' => 'Adriano', 'surname' => 'Placido', 'gender' => 'NB']
 ];
 
-$words2 = [
-  'elemento1' => 25.89,
-  'elemento2' => 'possa',
-  'elemento3' => [
-    'Virgilio',
-    'Favore',
-    'favore',
-    ['fortuna']
-  ],
- 'fine' => '!'
-];
-
-
-// Crea una variabile di tipo Stringa chiamata $results che stampi a terminare il seguente testo, attraverso l’accesso agli array sopra: “Nel mezzo di cammin di nostra vita mi ritrovai per una selva oscura, che' la diritta via era smarrita“.
-
-// HINT:
-// Non tutti gli elementi all’interno dell’array sono utili, e alcuni elementi mancano proprio. Scegli come risolvere il problema nel modo piu' opportuno: nuove variabili, concatenamenti, ecc ecc.
-
-
-$results = "Nel " . $words1[6][3][0] . " di cammin di nostra vita mi ritrovai per una selva " . $words1[6][0] . ", che' la diritta via era smarrita" . $words2['fine'];
-
-echo $results;
+foreach ($users as $user) {
+  if ($user['gender'] == 'M') {
+    echo "Buongiorno Sig. " . $user['name'] . " " . $user['surname'] . "\n";
+  } elseif ($user['gender'] == 'F') {
+    echo "Buongiorno Sig.ra " . $user['name'] . " " . $user['surname'] . "\n";
+  } else {
+    echo "Buongiorno " . $user['name'] . " " . $user['surname'] . "\n";
+  }   ;
+};
